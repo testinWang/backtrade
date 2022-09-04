d74f922dacc3d76b3ddef394ffa9cfd49a8d6496
testinWang <18810815820@163.com>
2022-09-04 17:58:08 +0800

    增加从东方财富获取主力资金流向的代码

---
+++ strategy/RankZX.py
@@ -0,0 +1,72 @@
+#本文件根据先验知识对自选股票进行排序
+import os
+import sys
+
+
+class RankZX:
+    def __init__(self):
+        self.base_data_path = r'C:\zd_zyb\T0002\blocknew'
+        self.zx_path = os.path.join(self.base_data_path, "ZXG.blk")
+
+    # 读取自选列表
+    def get_zx_code_list(self):
+        with open(self.zx_path, encoding="utf-8") as f:
+            code_str_line = f.readlines()
+            # 自选股的存储第一位表示沪深，后六位才是真正的股票代码
+            code_list = []
+            for code_str in code_str_line:
+                code_str = str(code_str).strip()
+                if len(code_str) == 7:
+                    market = code_str[0]
+                    code = code_str[1:7]
+                    code_dict = {'market': market, 'code': code}
+                    code_list.append(code_dict)
+        return code_list
+
+    # 将重新计算好的code_list回写自选文件
+    def rewrite_zx_data(self, code_list):
+        with open(self.zx_path, encoding="utf-8", mode="w") as f:
+            write_lines = []
+            for code_dict in code_list:
+                market = code_dict['market']
+                code = code_dict['code']
+                write_lines.append(str(market)+str(code))
+            f.write('\n'.join(write_lines))
+
+    # 策略一 有涨停但是持续阴跌的票，放最后
+    def yindie(self, code):
+        pass
+
+    # 策略二： 涨停后次日放巨量收阴线， 放最后， 巨量：超过涨停日成交量150%
+    def huge_yinxian(self, code):
+        pass
+
+    # 策略三：连续三日内收阴，且三日没有一日最高涨幅超过3%的 ，放最后
+    def three_date_yinxian(self, code):
+        pass
+
+    # 策略四：最近有摁地板，
+    def dieting(self, code):
+        pass
+
+    # 连续三日下跌放巨量
+    def xiadie_huge_vol(self,code):
+
+        pass
+
+    # 连续三日以上阳线，首次出现阴线
+    def three_yanxian(self, code):
+        pass
+
+    
+
+
+
+
+
+
+
+
+
+
+
